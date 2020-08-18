c23827f159d986940361c31979560108c078960a
Andrey Sakharov <a.sakharov@biletix.ru>
2020-08-18 03:51:14 +0300

    looks like HousesParser implemented

--- src/parser.spec.ts
+++ src/parser.spec.ts
@@ -2,9 +2,231 @@ import { HousesParser } from './parser';
 
 describe('HousesParser', () =>
 {
-   test('first test', () =>
+   test('четные', () =>
    {
-      const parser = new HousesParser('четные 2-28, нечетные 1-21');
+      const parser = new HousesParser('четные');
+      expect(parser.isHouseIncluded('18')).toBe(true);
+      expect(parser.isHouseIncluded('17')).toBe(false);
+      expect(parser.isHouseIncluded('18а')).toBe(true);
+      expect(parser.isHouseIncluded('17а')).toBe(false);
+      expect(parser.isHouseIncluded('18/1')).toBe(true);
+      expect(parser.isHouseIncluded('17/1')).toBe(false);
+   });
+
+   test('нечетные', () =>
+   {
+      const parser = new HousesParser('нечетные');
       expect(parser.isHouseIncluded('18')).toBe(false);
+      expect(parser.isHouseIncluded('17')).toBe(true);
+   });
+
+   test('нечетные 11+, четные 42+', () =>
+   {
+      const parser = new HousesParser('нечетные 11+, четные 42+');
+      expect(parser.isHouseIncluded('18')).toBe(false);
+      expect(parser.isHouseIncluded('58')).toBe(true);
+      expect(parser.isHouseIncluded('9')).toBe(false);
+      expect(parser.isHouseIncluded('12')).toBe(false);
+      expect(parser.isHouseIncluded('17')).toBe(true);
+   });
+
+   test('нечетные 11-, четные 42-', () =>
+   {
+      const parser = new HousesParser('нечетные 11-, четные 42-');
+      expect(parser.isHouseIncluded('1')).toBe(true);
+      expect(parser.isHouseIncluded('2')).toBe(true);
+      expect(parser.isHouseIncluded('13')).toBe(false);
+      expect(parser.isHouseIncluded('14')).toBe(true);
+      expect(parser.isHouseIncluded('43')).toBe(false);
+   });
+
+   test('четные 11-15, четные 42-45', () =>
+   {
+      const parser = new HousesParser('четные 11-15, четные 42-45');
+      expect(parser.isHouseIncluded('10')).toBe(false);
+      expect(parser.isHouseIncluded('11')).toBe(false);
+      expect(parser.isHouseIncluded('12')).toBe(true);
+      expect(parser.isHouseIncluded('16')).toBe(false);
+      expect(parser.isHouseIncluded('42')).toBe(true);
+      expect(parser.isHouseIncluded('46')).toBe(false);
+   });
+
+   test('четные с 20 и вся улица до конца', () =>
+   {
+      const parser = new HousesParser('четные с 20 и вся улица до конца');
+      expect(parser.isHouseIncluded('18')).toBe(false);
+      expect(parser.isHouseIncluded('19')).toBe(false);
+      expect(parser.isHouseIncluded('20')).toBe(true);
+      expect(parser.isHouseIncluded('21')).toBe(false);
+      expect(parser.isHouseIncluded('22')).toBe(true);
+   });
+
+   test('нечетные с 20 и вся улица до конца', () =>
+   {
+      const parser = new HousesParser('нечетные с 20 и вся улица до конца');
+      expect(parser.isHouseIncluded('19')).toBe(false);
+      expect(parser.isHouseIncluded('20')).toBe(false);
+      expect(parser.isHouseIncluded('21')).toBe(true);
+      expect(parser.isHouseIncluded('22')).toBe(false);
+      expect(parser.isHouseIncluded('23')).toBe(true);
+   });
+
+   test('с 20 и вся улица до конца', () =>
+   {
+      const parser = new HousesParser('с 20 и вся улица до конца');
+      expect(parser.isHouseIncluded('19')).toBe(false);
+      expect(parser.isHouseIncluded('20')).toBe(true);
+      expect(parser.isHouseIncluded('21')).toBe(true);
+      expect(parser.isHouseIncluded('22')).toBe(true);
+   });
+
+   test('7/1, 11, 17, 17/1, 17/2, 8/2, 15, 15/1, 15а', () =>
+   {
+      const parser = new HousesParser('7/1, 11, 17, 17/1, 17/2, 8/2, 15, 15/1, 15а');
+      expect(parser.isHouseIncluded('18')).toBe(false);
+      expect(parser.isHouseIncluded('11')).toBe(true);
+      expect(parser.isHouseIncluded('11/1')).toBe(false);
+      expect(parser.isHouseIncluded('17/1')).toBe(true);
+      expect(parser.isHouseIncluded('15а')).toBe(true);
+      expect(parser.isHouseIncluded('15б')).toBe(false);
+   });
+
+   test('12, 22, 36, 42, 45, 100-106', () =>
+   {
+      const parser = new HousesParser('12, 22, 36, 42, 45, 100-106');
+      expect(parser.isHouseIncluded('11')).toBe(false);
+      expect(parser.isHouseIncluded('12')).toBe(true);
+      expect(parser.isHouseIncluded('13')).toBe(false);
+      expect(parser.isHouseIncluded('101')).toBe(true);
+      expect(parser.isHouseIncluded('107')).toBe(false);
+   });
+
+
+   describe('разгые варианты разделителей', () =>
+   {
+      test('четные 10-28, нечетные 5-21', () =>
+      {
+         const parser = new HousesParser('четные 10-28, нечетные 5-21');
+         expect(parser.isHouseIncluded('1')).toBe(false);
+         expect(parser.isHouseIncluded('2')).toBe(false);
+         expect(parser.isHouseIncluded('5')).toBe(true);
+         expect(parser.isHouseIncluded('6')).toBe(false);
+         expect(parser.isHouseIncluded('11')).toBe(true);
+         expect(parser.isHouseIncluded('12')).toBe(true);
+         expect(parser.isHouseIncluded('22')).toBe(true);
+         expect(parser.isHouseIncluded('23')).toBe(false);
+         expect(parser.isHouseIncluded('29')).toBe(false);
+      });
+
+      test('четные 10-28; нечетные 5-21', () =>
+      {
+         const parser = new HousesParser('четные 10-28; нечетные 5-21');
+         expect(parser.isHouseIncluded('1')).toBe(false);
+         expect(parser.isHouseIncluded('2')).toBe(false);
+         expect(parser.isHouseIncluded('5')).toBe(true);
+         expect(parser.isHouseIncluded('6')).toBe(false);
+         expect(parser.isHouseIncluded('11')).toBe(true);
+         expect(parser.isHouseIncluded('12')).toBe(true);
+         expect(parser.isHouseIncluded('22')).toBe(true);
+         expect(parser.isHouseIncluded('23')).toBe(false);
+         expect(parser.isHouseIncluded('29')).toBe(false);
+      });
+
+      test('четные 10-28 нечетные 5-21', () =>
+      {
+         const parser = new HousesParser('четные 10-28 нечетные 5-21');
+         expect(parser.isHouseIncluded('1')).toBe(false);
+         expect(parser.isHouseIncluded('2')).toBe(false);
+         expect(parser.isHouseIncluded('5')).toBe(true);
+         expect(parser.isHouseIncluded('6')).toBe(false);
+         expect(parser.isHouseIncluded('11')).toBe(true);
+         expect(parser.isHouseIncluded('12')).toBe(true);
+         expect(parser.isHouseIncluded('22')).toBe(true);
+         expect(parser.isHouseIncluded('23')).toBe(false);
+         expect(parser.isHouseIncluded('29')).toBe(false);
+      });
+
+      test('четные10-28нечетные5-21', () =>
+      {
+         const parser = new HousesParser('четные10-28нечетные5-21');
+         expect(parser.isHouseIncluded('1')).toBe(false);
+         expect(parser.isHouseIncluded('2')).toBe(false);
+         expect(parser.isHouseIncluded('5')).toBe(true);
+         expect(parser.isHouseIncluded('6')).toBe(false);
+         expect(parser.isHouseIncluded('11')).toBe(true);
+         expect(parser.isHouseIncluded('12')).toBe(true);
+         expect(parser.isHouseIncluded('22')).toBe(true);
+         expect(parser.isHouseIncluded('23')).toBe(false);
+         expect(parser.isHouseIncluded('29')).toBe(false);
+      });
+   });
+
+
+   describe('ошибки валидации', () =>
+   {
+      test('в конструктор передали не строку', () =>
+      {
+         try {
+            // @ts-expect-error
+            const parser42 = new HousesParser(42);
+         }
+         catch (err) {
+            expect(err.message).toBe('the source should be a string');
+         }
+      });
+
+      test('в конструктор передали данные, не содержашие информацию о номерах домов', () =>
+      {
+         try {
+            // @ts-expect-error
+            const milkParser = new HousesParser('молоко');
+         }
+         catch (err) {
+            expect(err.message).toBe('the source should contain numbers or even/odd modifiers');
+         }
+      });
+
+      const parser = new HousesParser('100500');
+
+      test('в метод передали не строку', () =>
+      {
+         try {
+            // @ts-expect-error
+            parser.isHouseIncluded(42);
+         }
+         catch (err) {
+            expect(err.message).toBe('the houseNumber should be a string');
+         }
+      });
+
+      test('в метод передали не числовую строку', () =>
+      {
+         try {
+            parser.isHouseIncluded('молоко');
+         }
+         catch (err) {
+            expect(err.message).toBe('incorrect houseNumber: молоко');
+         }
+      });
+
+      test('в метод передали отрицательный номер дома', () =>
+      {
+         try {
+            parser.isHouseIncluded('-1');
+         }
+         catch (err) {
+            expect(err.message).toBe('incorrect houseNumber: -1');
+         }
+      });
+
+      test('в метод передали нецелый номер дома', () =>
+      {
+         try {
+            parser.isHouseIncluded('1.1');
+         }
+         catch (err) {
+            expect(err.message).toBe('the houseNumber should be an integer');
+         }
+      });
    });
 });
--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,259 @@
+type TCheck = (houseNumber: string, justNumbersPart: number) => boolean;
+
+interface ICheckOddResult {
+   canIncludesOdd?: string;
+   canNotIncludesOdd?: string;
+}
+
+interface ICheckEvenResult {
+   canIncludesEven?: string;
+   canNotIncludesEven?: string;
+}
+
 
 export class HousesParser
 {
-   constructor(
-      private readonly _source: string,
-   ) {}
+   private static _EVEN = 'четные';
+   private static _EVEN_RE = new RegExp(HousesParser._EVEN);
+   private static _ODD  = 'нечетные';
+   private static _ODD_RE  = new RegExp(HousesParser._ODD);
+   private static _SPACES_RE  = /\s/g;
+   private static _NUMBER_RE  = /\d/;
+   private static _RULES_RE  = /\d+(\/\d+|[a-zа-я]+|\+|\-(\d+)?|\s[\sa-zа-я]* до конца)?/g;
+
+   private readonly _oddChecks: TCheck[] = [];
+   private readonly _evenChecks: TCheck[] = [];
+   private readonly _allChecks: TCheck[] = [];
+
+
+
+   constructor(source: string)
+   {
+      if (typeof source !== 'string') {
+         throw new Error('the source should be a string');
+      }
+
+      const processedSource = source
+         .trim()
+         .replace(/\s+/g, ' ')
+         .toLowerCase()
+         .replace(/ё/g, 'е');
+
+      if (!/\d/.test(processedSource) && !processedSource.includes(HousesParser._EVEN)) {
+         throw new Error('the source should contain numbers or even/odd modifiers');
+      }
+
+      this._init(processedSource);
+   }
+
+
+   //  *********************************
+   //  *                               *
+   //  *        Public Methods         *
+   //  *                               *
+   //  *********************************
 
    public isHouseIncluded(houseNumber: string): boolean
    {
-      return this._source.includes(houseNumber);
+      if (typeof houseNumber !== 'string') {
+         throw new Error('the houseNumber should be a string');
+      }
+
+      const processedHouseNumber = houseNumber
+         .replace(HousesParser._SPACES_RE, '')
+         .toLowerCase();
+
+      const numericPart = this._parseInt(processedHouseNumber);
+
+      return numericPart % 2 === 0
+         ? this._evenChecks.some(c => c(processedHouseNumber, numericPart)) || this._allChecks.some(c => c(processedHouseNumber, numericPart))
+         : this._oddChecks.some(c => c(processedHouseNumber, numericPart)) || this._allChecks.some(c => c(processedHouseNumber, numericPart));
    }
+
+
+
+   //  *********************************
+   //  *                               *
+   //  *        Private Methods        *
+   //  *                               *
+   //  *********************************
+
+   private _init(str: string): void
+   {
+      const {canIncludesOdd, canNotIncludesOdd} = this._getOddSubstrings(str);
+
+      if (canIncludesOdd !== undefined) {
+         (() => {
+            const {canIncludesEven, canNotIncludesEven} = this._getEvenSubstrings(canIncludesOdd);
+            if (canNotIncludesEven !== undefined) {
+               this._setChecks(this._oddChecks, canNotIncludesEven, 'odd');
+            }
+            if (canIncludesEven !== undefined) {
+               this._setChecks(this._evenChecks, canIncludesEven, 'even');
+            }
+         })();
+      }
+
+      if (canNotIncludesOdd !== undefined) {
+         const {canIncludesEven, canNotIncludesEven} = this._getEvenSubstrings(canNotIncludesOdd);
+
+         if (canIncludesEven !== undefined) {
+            this._setChecks(this._evenChecks, canIncludesEven, 'even');
+         }
+
+         if (canNotIncludesEven !== undefined) {
+            this._setChecks(this._allChecks, canNotIncludesEven);
+         }
+      }
+   }
+
+
+   private _getOddSubstrings(str: string): ICheckOddResult
+   {
+      const processedStr = str.trim();
+      const pos = processedStr.search(HousesParser._ODD_RE);
+      const result: ICheckOddResult = {};
+
+      switch (pos) {
+         case -1:
+            result.canNotIncludesOdd = processedStr;
+            break;
+
+         case 0:
+            result.canIncludesOdd = processedStr.substr(pos + HousesParser._ODD.length);
+            break;
+
+         default:
+            result.canIncludesOdd = processedStr.substr(pos + HousesParser._ODD.length);
+            result.canNotIncludesOdd = processedStr.substr(0, pos);
+      }
+
+      return result;
+   }
+
+   private _getEvenSubstrings(str: string): ICheckEvenResult
+   {
+      const processedStr = str.trim();
+      const pos = processedStr.search(HousesParser._EVEN_RE);
+      const result: ICheckEvenResult = {};
+
+      switch (pos) {
+         case -1:
+            result.canNotIncludesEven = processedStr;
+            break;
+
+         case 0:
+            result.canIncludesEven = processedStr.substr(pos + HousesParser._EVEN.length);
+            break;
+
+         default:
+            result.canIncludesEven = processedStr.substr(pos + HousesParser._EVEN.length);
+            result.canNotIncludesEven = processedStr.substr(0, pos);
+      }
+
+      return result;
+   }
+
+
+   private _setChecks(checkArr: TCheck[], onlyNumbersStr: string, flag?: 'odd' | 'even'): void
+   {
+      if (onlyNumbersStr === '') {
+         flag === 'odd'
+            ? checkArr.push(this._setJustOddCheck())
+            : checkArr.push(this._setJustEvenCheck());
+         return;
+      }
+
+      const rules = onlyNumbersStr.match(HousesParser._RULES_RE);
+
+      if (rules !== null)
+      {
+         const notEmptyRules = rules
+            .filter(r => r.search(HousesParser._NUMBER_RE) !== -1);
+
+         const simpleRules: string[] = [];
+
+         notEmptyRules.forEach(r => {
+            switch (true)
+            {
+               case r[r.length - 1] === '+':
+               case r.includes('до конца'):
+                  const minInt = this._parseInt(r);
+                  checkArr.push(this._setIsGreaterOrEqualCheck(minInt));
+                  break;
+
+               case r[r.length - 1] === '-':
+                  const maxInt = this._parseInt(r);
+                  checkArr.push(this._setIsLessOrEqualCheck(maxInt));
+                  break;
+
+               case r.includes('-'):
+                  const [minStr, maxStr] = r.split('-');
+                  const min = this._parseInt(minStr);
+                  const max = this._parseInt(maxStr);
+                  checkArr.push(this._setIsInDiapasonCheck(min, max));
+                  break;
+
+               default:
+                  simpleRules.push(r);
+            }
+         });
+
+         if (simpleRules.length > 0) {
+            checkArr.push(this._setIsEqualCheck(simpleRules));
+         }
+      }
+   }
+
+
+
+
+   private _setIsGreaterOrEqualCheck(num: number): TCheck
+   {
+      return (_houseNumber: string, justNumbersPart: number) => justNumbersPart >= num;
+   }
+
+   private _setIsLessOrEqualCheck(num: number): TCheck
+   {
+      return (_houseNumber: string, justNumbersPart: number) => justNumbersPart <= num;
+   }
+
+   private _setIsInDiapasonCheck(min: number, max: number): TCheck
+   {
+      return (_houseNumber: string, justNumbersPart: number) => justNumbersPart >= min && justNumbersPart <= max;
+   }
+
+   private _setIsEqualCheck(numbers: string[]): TCheck
+   {
+      return (houseNumber: string, _justNumbersPart: number) => {
+         return numbers.includes(houseNumber);
+      };
+   }
+
+   private _setJustEvenCheck(): TCheck
+   {
+      return (_houseNumber: string, justNumbersPart: number) => justNumbersPart % 2 === 0;
+   }
+
+   private _setJustOddCheck(): TCheck
+   {
+      return (_houseNumber: string, justNumbersPart: number) => justNumbersPart % 2 !== 0;
+   }
+
+
+   private _parseInt(str: string): number
+   {
+      const numericPart = parseInt(str, 10);
+      if (Number.isNaN(numericPart) || numericPart <= 0) {
+         throw new Error(`incorrect houseNumber: ${str}`);
+      }
+
+      const floatPart = parseFloat(str);
+      if (floatPart !== numericPart) {
+         throw new Error('the houseNumber should be an integer');
+      }
+
+      return numericPart;
+   }
+
 }
